4a0e4dd138fd3b1493963e5c9aa85dbd3963a6a6
RodrigoQueiros <rodrigodlq@hotmail.com>
2019-02-23 15:56:26 +0000

    aaa

--- Game/loadImages.js
+++ Game/loadImages.js
@@ -53,11 +53,76 @@ let images = {
                     downsidebridge: new Image()
                 }
             }
+        },
+
+        two: {
+            upside: {
+                type: {
+                    one: {
+                        variantOne: new Image(),
+                        variantTwo: new Image(),
+                        variantThree: new Image()
+                    },
+                    two: {
+                        variantOne: new Image(),
+                        variantTwo: new Image(),
+                        variantThree: new Image()
+                    }
+                },
+                checkPoint: {
+                    upsidecheckpointOne: new Image()
+                },
+                bridge: {
+                    upsidebridge: new Image()
+                }
+            },
+            downside: {
+                type: {
+                    one: {
+                        variantOne: new Image(),
+                        variantTwo: new Image(),
+                        variantThree: new Image()
+                    },
+                    two: {
+                        variantOne: new Image(),
+                        variantTwo: new Image(),
+                        variantThree: new Image()
+                    }
+                },
+                checkPoint: {
+                    downsidecheckpointOne: new Image()
+                },
+                bridge: {
+                    downsidebridge: new Image()
+                }
+            },
+            portal: new Image(),
         }
         
     },
     enemies: {
         one:{
+            upside: {
+                three: {
+                    upsidehazardspike: new Image(),
+                    upsidehazardspikeTwo: new Image(),
+                },
+                five: {
+                    upsidehazardflyer: new Image(),
+                }
+            },
+            downside: {
+                three: {
+                    downsidehazardspike: new Image(),
+                    downsidehazardspikeTwo: new Image(),
+                },
+                five: {
+                    downsidehazardflyer: new Image(),
+                }
+            }
+
+        },
+        two:{
             upside: {
                 three: {
                     upsidehazardspike: new Image(),
@@ -118,3 +183,38 @@ images.enemies.one.downside.five.downsidehazardflyer.src = "img/enemies/one/down
 images.tiles.one.upside.checkPoint.upsidecheckpointOne.src = "img/tiles/one/upside/checkPoint/upsidecheckpointOne.png"
 images.tiles.one.downside.checkPoint.downsidecheckpointOne.src = "img/tiles/one/downside/checkPoint/downsidecheckpointOne.png"
 images.tiles.one.downside.bridge.downsidebridge.src = "img/tiles/one/downside/bridge/downsidebridge.png"
+
+//two
+
+images.tiles.two.upside.type.one.variantOne.src = "img/tiles/two/upside/type/one/variantOne.png"
+images.tiles.two.upside.type.one.variantTwo.src = "img/tiles/two/upside/type/one/variantTwo.png"
+images.tiles.two.upside.type.one.variantThree.src = "img/tiles/two/upside/type/one/variantThree.png"
+
+images.tiles.two.upside.type.two.variantOne.src = "img/tiles/two/upside/type/two/variantOne.png"
+images.tiles.two.upside.type.two.variantTwo.src = "img/tiles/two/upside/type/two/variantTwo.png"
+images.tiles.two.upside.type.two.variantThree.src = "img/tiles/two/upside/type/two/variantThree.png"
+
+images.tiles.two.downside.type.one.variantOne.src = "img/tiles/two/downside/type/one/variantOne.png"
+images.tiles.two.downside.type.one.variantTwo.src = "img/tiles/two/downside/type/one/variantTwo.png"
+images.tiles.two.downside.type.one.variantThree.src = "img/tiles/two/downside/type/one/variantThree.png"
+
+images.tiles.two.downside.type.two.variantOne.src = "img/tiles/two/downside/type/two/variantOne.png"
+images.tiles.two.downside.type.two.variantTwo.src = "img/tiles/two/downside/type/two/variantTwo.png"
+images.tiles.two.downside.type.two.variantThree.src = "img/tiles/two/downside/type/two/variantThree.png"
+
+images.enemies.two.upside.three.upsidehazardspike.src = "img/enemies/two/upside/three/upsidehazardspike.png"
+
+
+images.enemies.two.downside.three.downsidehazardspike.src = "img/enemies/two/downside/three/downsidehazardspikeTwo.png"
+
+
+images.enemies.two.upside.five.upsidehazardflyer.src = "img/enemies/two/upside/five/upsidehazardflyer.png"
+images.enemies.two.downside.five.downsidehazardflyer.src = "img/enemies/two/downside/five/downsidehazardflyer.png"
+
+images.tiles.two.upside.checkPoint.upsidecheckpointOne.src = "img/tiles/two/upside/checkPoint/upsidecheckpointOne.png"
+images.tiles.two.downside.checkPoint.downsidecheckpointOne.src = "img/tiles/two/downside/checkPoint/downsidecheckpointOne.png"
+images.tiles.two.downside.bridge.downsidebridge.src = "img/tiles/two/downside/bridge/downsidebridge.png"
+images.tiles.two.upside.bridge.upsidebridge.src = "img/tiles/two/upside/bridge/upsidebridge.png"
+
+images.tiles.two.portal.src = "img/tiles/two/portal.png"
+
--- Game/objects/platform.js
+++ Game/objects/platform.js
@@ -34,10 +34,8 @@ class Platform {
             context.fillStyle = "black"
         }*/
 
-
-
-        
-        //plataforms upside
+        if(this.lvl==1){
+            //plataforms upside
         if (this.type == 1 && this.upside) {
 
             switch (this.rand) {
@@ -147,6 +145,122 @@ class Platform {
             context.drawImage(images.tiles.one.downside.bridge.downsidebridge, this.x, this.y, this.w, this.h)
         }
 
+
+        }
+
+        if(this.lvl==2){
+            //plataforms upside
+        if (this.type == 1 && this.upside) {
+
+            switch (this.rand) {
+                case 1:
+                    context.drawImage(images.tiles.two.upside.type.one.variantOne, this.x, this.y, this.w, this.h)
+                    break
+                case 2:
+                    context.drawImage(images.tiles.two.upside.type.one.variantTwo, this.x, this.y, this.w, this.h)
+                    break
+                case 3:
+                    context.drawImage(images.tiles.two.upside.type.one.variantThree, this.x, this.y, this.w, this.h)
+                    break
+            }
+
+        } else if (this.type == 2 && this.upside) {
+            switch (this.rand) {
+                case 1:
+                    context.drawImage(images.tiles.two.upside.type.two.variantOne, this.x, this.y, this.w, this.h)
+                    break
+                case 2:
+                    context.drawImage(images.tiles.two.upside.type.two.variantTwo, this.x, this.y, this.w, this.h)
+                    break
+                case 3:
+                    context.drawImage(images.tiles.two.upside.type.two.variantThree, this.x, this.y, this.w, this.h)
+                    break
+            }
+        }
+
+        //plataforms downside
+        if (this.type == 1 && !this.upside) {
+
+            switch (this.rand) {
+                case 1:
+                    context.drawImage(images.tiles.two.downside.type.one.variantOne, this.x, this.y, this.w, this.h)
+                    break
+                case 2:
+                    context.drawImage(images.tiles.two.downside.type.one.variantTwo, this.x, this.y, this.w, this.h)
+                    break
+                case 3:
+                    context.drawImage(images.tiles.two.downside.type.one.variantThree, this.x, this.y, this.w, this.h)
+                    break
+            }
+        } else if (this.type == 2 && !this.upside) {
+            switch (this.rand) {
+                case 1:
+                    context.drawImage(images.tiles.two.downside.type.two.variantOne, this.x, this.y, this.w, this.h)
+                    break
+                case 2:
+                    context.drawImage(images.tiles.two.downside.type.two.variantTwo, this.x, this.y, this.w, this.h)
+                    break
+                case 3:
+                    context.drawImage(images.tiles.two.downside.type.two.variantThree, this.x, this.y, this.w, this.h)
+                    break
+            }
+        }
+        //Monsters down
+        if (this.type == 3 && !this.upside) {
+
+            context.drawImage(images.enemies.two.downside.three.downsidehazardspike, this.x, this.y, this.w, this.h)
+
+        } else if (this.type == 5 && !this.upside) {
+            context.drawImage(images.enemies.two.downside.five.downsidehazardflyer, 0 + this.currFrame, 0, this.w, this.h, this.x, this.y, this.w, this.h);
+            if (frame % 10 === 0) {
+                this.currFrame += 50
+            }
+
+
+            if (this.currFrame > 150) {
+                this.currFrame = 0
+            }
+        }
+
+        //Monsters upside
+        if (this.type == 3 && this.upside) {
+
+            context.drawImage(images.enemies.two.upside.three.upsidehazardspike, this.x, this.y, this.w, this.h)
+
+        } else if (this.type == 5 && this.upside) {
+
+
+            context.drawImage(images.enemies.two.upside.five.upsidehazardflyer, 0 + this.currFrame, 0, this.w, this.h, this.x, this.y, this.w, this.h);
+
+            if (frame % 10 === 0) {
+                this.currFrame += 50
+            }
+
+
+            if (this.currFrame > 100) {
+                this.currFrame = 0
+            }
+        }
+
+        //Bridge
+        if (this.type == 4 && !this.upside) {
+            context.drawImage(images.tiles.two.downside.bridge.downsidebridge, this.x, this.y, this.w, this.h)
+        }
+        if (this.type == 4 && this.upside) {
+            context.drawImage(images.tiles.two.upside.bridge.upsidebridge, this.x, this.y, this.w, this.h)
+        }
+        if (this.type == 9 && this.upside) {
+            context.drawImage(images.tiles.two.portal,0,0,50,50, this.x, this.y, this.w, this.h)
+        }
+        if (this.type == 9 && !this.upside) {
+            context.drawImage(images.tiles.two.portal,0,50,50,50, this.x, this.y, this.w, this.h)
+        }
+
+
+        }
+
+        
+        
     }
 
 }
